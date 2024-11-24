1f7a956bfb46d139990dece9881de49af4dcb385
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-11-24 20:02:46 +0600

    task3 MVC done

---
+++ task3/model/model.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.ArrayList;
+
+class member {
+    //
+}
+
+class socialMediaPost {
+    //
+}
+
+public class model {
+    ArrayList<member> list_of_members = new ArrayList<>();
+    ArrayList<socialMediaPost> list_of_posts = new ArrayList<>();
+
+    public model() { // constructor
+        //
+    }
+
+    public void addSocialMediaPost() {
+        //
+    }
+
+    public boolean isEligibleForContest() { // business logic
+        boolean isEligible = true;
+//        if (){
+//            //
+//        } else {
+//            //
+//        }
+        return isEligible;
+    }
+
+    public void judgeSubmissions() {
+        //
+    }
+
+    public void receiveSubmissions() {
+        //
+    }
+
+    public void formCommittee() {
+        //
+    }
+
+}
