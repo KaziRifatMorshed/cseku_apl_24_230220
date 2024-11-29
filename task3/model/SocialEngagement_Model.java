21483b2a368b1117039d1470d0d3c414420e756e
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-11-30 02:37:08 +0600

    updated

--- task3/model/SocialEngagement_Model.java
+++ task3/model/SocialEngagement_Model.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class SocialEngagement_Model {
     // model contains data
-    ArrayList<SocialMediaPost> list_of_posts = new ArrayList<>();
+    ArrayList<SocialMediaPost> list_of_posts = new ArrayList<>(); // databases of previous posts
 
     public SocialEngagement_Model() { // constructor
         //
